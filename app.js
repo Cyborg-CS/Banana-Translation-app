3ff82cabfb891e270973cd90658036bb0328c716
DevMD <81987904+Cyborg-CS@users.noreply.github.com>
2021-11-23 21:42:35 +0530

    add the script

---
+++ app.js
@@ -0,0 +1,32 @@
+var userAction = document.querySelector("#btn-translate");
+var userText = document.querySelector("#user-text")
+var outputText = document.querySelector("#output-box")
+
+var requestURL = "https://api.funtranslations.com/translate/pirate.json"
+
+
+function errorHandler(error) {
+  console.log(`error occured ${error}`)
+  alert("Server down ! Try it again in sometimes...")
+}
+
+function constructURL(text) {
+  return `${requestURL}?text=${text}`;
+}
+
+
+function clickEventHandler() {
+  var inputText = userText.value;
+
+  fetch(constructURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+      var translatedText = json.contents.translated
+      outputText.innerText = translatedText;
+    })
+
+    .catch(errorHandler)
+
+}
+
+userAction.addEventListener("click", clickEventHandler);
